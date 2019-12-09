5424a95b53825dace3268a0f4fe49efaa7f165b7
Ben Weinberg <weinbergben33@gmail.com>
2019-12-09 17:02:44 -0500

    test push

--- socketServices.js
+++ socketServices.js
@@ -1,30 +1,29 @@
 let _io;
 
-
 let listen = socket => {
-    const io = _io
-    console.log(socket.id)
-    socket.on('ready', ()=> {
-        console.log("READY", socket.id)
-        socket.broadcast.emit('ready', socket.id)
-    })
+  const io = _io;
+  console.log(socket.id, "socket.id");
+  socket.on("ready", () => {
+    console.log("READY", socket.id);
+    socket.broadcast.emit("ready", socket.id);
+  });
 
-    socket.on("offer", (id, message) => {
-        console.log('offer received')
-        socket.to(id).emit("offer",socket.id, message )
-    })
+  socket.on("offer", (id, message) => {
+    console.log("offer received");
+    socket.to(id).emit("offer", socket.id, message);
+  });
 
-    socket.on("answer", (id, message) => {
-        console.log('answer received')
-        socket.to(id).emit('answer', socket.id, message)
-    })
+  socket.on("answer", (id, message) => {
+    console.log("answer received");
+    socket.to(id).emit("answer", socket.id, message);
+  });
 
-    socket.on('candidate',  (id, message) => {
-        socket.to(id).emit('candidate', socket.id, message);
-      });
-}
+  socket.on("candidate", (id, message) => {
+    socket.to(id).emit("candidate", socket.id, message);
+  });
+};
 
 module.exports = function(io) {
-    _io = io
-    return {listen}
-}
+  _io = io;
+  return { listen };
+};
