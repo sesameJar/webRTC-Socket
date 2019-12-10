a5af00b43a328b3a0be6a7b06986a06e3913d754
Mehrad Kavian <mehrad.kavian@gmail.com>
2019-12-10 10:54:59 -0500

    working on the call approach

--- public/js/index.js
+++ public/js/index.js
@@ -1,98 +1,152 @@
-let socket = io()
+let socket = io();
 
-let localVideo = document.querySelector('.localVideo')
-let remoteVideos = document.querySelector('.remoteVideos')
+let localVideo = document.querySelector(".localVideo");
+let remoteVideos = document.querySelector(".remoteVideos");
 // let remoteVideoTag = document.getElementById("remoteVideoTag")
-let peerConnections ={}
+let submitBtn = document.querySelector(".sendAvatar");
+let avatarTag = document.querySelector("#avatar");
+let usersList = document.querySelector(".usersList");
+let peerConnections = {};
 
 const config = {
-    'iceServers' : [{
-        'urls' : ['stun:stun.1.google.com:19302']
-    }]
-}
+  iceServers: [
+    {
+      urls: ["stun:stun.1.google.com:19302"]
+    }
+  ]
+};
 
 const constraints = {
-    video : true,
-    audio : true
-}
+  video: true,
+  audio: true
+};
 
-socket.on('ready', async id => {
-    if(!(localVideo instanceof HTMLVideoElement) || !localVideo.srcObject) {
-        return
-    }
-    
-    const peerConnection = new RTCPeerConnection(config)
-    peerConnections[id] = peerConnection
-    
-    peerConnection.addStream(localVideo.srcObject)
+submitBtn.addEventListener("click", event => {
+  socket.emit("avatar", avatarTag.value);
+  console.log(avatarTag.value);
+});
 
-    let SDP = await peerConnection.createOffer()
-    await peerConnection.setLocalDescription(SDP)
-    socket.emit('offer', id, peerConnection.localDescription)
+socket.on("all-users", async users => {
+  usersList.innerHTML = "";
+  for (user in users) {
+    let li = document.createElement("li");
+    li.innerHTML = `User ${user} has id : ${users[user]}`;
+    usersList.appendChild(li);
+    li.addEventListener("click", async event => {
+      let id = users[user];
+      await getMediaDevices(id);
+      await createOffer(id);
+    });
+  }
+});
 
-    peerConnection.onaddstream = event => handleRemoteStreamAdded(event.stream, id)
-    peerConnection.onicecandidate = function(event) {
-        if (event.candidate) {
-          socket.emit('candidate', id, event.candidate);
-        }
-    };
-
-})
-
-socket.on('offer', async (id, offer) => {
-    const peerConnection = new RTCPeerConnection(config)
-    peerConnections[id] = peerConnection
-    peerConnection.addStream(localVideo.srcObject)
-    
-
-    peerConnection.setRemoteDescription(offer) // !! AWAITS NO NEEDED FOR REMOTE DESCRIPTION
-    let SDP = await peerConnection.createAnswer()
-    peerConnection.setLocalDescription(SDP)
-
-    socket.emit('answer',id, peerConnection.localDescription)
-    peerConnection.onaddstream = event => handleRemoteStreamAdded(event.stream, id)
-    peerConnection.onicecandidate = function(event) {
-        if (event.candidate) {
-          socket.emit('candidate', id, event.candidate);
-        }
-    };
-})
-
-socket.on('answer',  (id, answer) => {
-    peerConnections[id].setRemoteDescription( answer)
-})
-
-socket.on('candidate', (id, candidate) => {
-    peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate))
-    .catch(e => console.error(e));
-  });
-
-let handleRemoteStreamAdded = (stream, id) => {
-    let mediaStream = new MediaStream(stream)
-    const remoteVideo = document.createElement('video');
-    remoteVideo.srcObject = mediaStream;
-    remoteVideo.setAttribute("id", id.replace(/[^a-zA-Z]+/g, "").toLowerCase());
-    remoteVideo.setAttribute("playsinline", "true");
-    remoteVideo.setAttribute("autoplay", "true");
-    remoteVideos.appendChild(remoteVideo);
-    if (remoteVideos.querySelectorAll("video").length === 1) {
-      remoteVideos.setAttribute("class", "one remoteVideos");
-    } else {
-      remoteVideos.setAttribute("class", "remoteVideos");
-    }
+const createOffer = async id => {
+  console.log("createOffer");
+  if (!(localVideo instanceof HTMLVideoElement) || !localVideo.srcObject) {
+    await getMediaDevices(id);
   }
 
-let getMediaDevices = async () => {
-    if(localVideo instanceof HTMLVideoElement) {
-        if(!localVideo.srcObject) {
-            let stream = await navigator.mediaDevices.getUserMedia(constraints)
-            localVideo.srcObject = stream
-            socket.emit('ready')
-        }
+  const peerConnection = new RTCPeerConnection(config);
+  peerConnections[id] = peerConnection;
 
-    }else {
-        console.error('NO VIDEO TAG FOUND')
+  peerConnection.addStream(localVideo.srcObject);
+
+  let SDP = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(SDP);
+  socket.emit("offer", id, peerConnection.localDescription);
+
+  peerConnection.onaddstream = event =>
+    handleRemoteStreamAdded(event.stream, id);
+  peerConnection.onicecandidate = function(event) {
+    if (event.candidate) {
+      socket.emit("candidate", id, event.candidate);
     }
-}
+  };
+  console.log("END OF OFFER");
+};
 
-getMediaDevices()
+// socket.on("ready", async id => {
+//   if (!(localVideo instanceof HTMLVideoElement) || !localVideo.srcObject) {
+//     return;
+//   }
+
+//   const peerConnection = new RTCPeerConnection(config);
+//   peerConnections[id] = peerConnection;
+
+//   peerConnection.addStream(localVideo.srcObject);
+
+//   let SDP = await peerConnection.createOffer();
+//   await peerConnection.setLocalDescription(SDP);
+//   socket.emit("offer", id, peerConnection.localDescription);
+
+//   peerConnection.onaddstream = event =>
+//     handleRemoteStreamAdded(event.stream, id);
+//   peerConnection.onicecandidate = function(event) {
+//     if (event.candidate) {
+//       socket.emit("candidate", id, event.candidate);
+//     }
+//   };
+// });
+
+socket.on("offer", async (id, offer) => {
+  console.log("OFFER RECEIVED CLIENT");
+  if (!(localVideo instanceof HTMLVideoElement) || !localVideo.srcObject) {
+    await getMediaDevices(id);
+  }
+
+  const peerConnection = new RTCPeerConnection(config);
+  peerConnections[id] = peerConnection;
+  peerConnection.addStream(localVideo.srcObject);
+
+  peerConnection.setRemoteDescription(offer); // !! AWAITS NOT NEEDED FOR REMOTE DESCRIPTION
+  let SDP = await peerConnection.createAnswer();
+  peerConnection.setLocalDescription(SDP);
+
+  socket.emit("answer", id, peerConnection.localDescription);
+  peerConnection.onaddstream = event =>
+    handleRemoteStreamAdded(event.stream, id);
+  peerConnection.onicecandidate = function(event) {
+    if (event.candidate) {
+      socket.emit("candidate", id, event.candidate);
+    }
+  };
+});
+
+socket.on("answer", (id, answer) => {
+  peerConnections[id].setRemoteDescription(answer);
+});
+
+socket.on("candidate", (id, candidate) => {
+  peerConnections[id]
+    .addIceCandidate(new RTCIceCandidate(candidate))
+    .catch(e => console.error(e));
+});
+
+let handleRemoteStreamAdded = (stream, id) => {
+  let mediaStream = new MediaStream(stream);
+  const remoteVideo = document.createElement("video");
+  remoteVideo.srcObject = mediaStream;
+  remoteVideo.setAttribute("id", id.replace(/[^a-zA-Z]+/g, "").toLowerCase());
+  remoteVideo.setAttribute("playsinline", "true");
+  remoteVideo.setAttribute("autoplay", "true");
+  remoteVideos.appendChild(remoteVideo);
+  if (remoteVideos.querySelectorAll("video").length === 1) {
+    remoteVideos.setAttribute("class", "one remoteVideos");
+  } else {
+    remoteVideos.setAttribute("class", "remoteVideos");
+  }
+};
+
+let getMediaDevices = async id => {
+  console.log(id);
+  if (localVideo instanceof HTMLVideoElement) {
+    if (!localVideo.srcObject) {
+      let stream = await navigator.mediaDevices.getUserMedia(constraints);
+      localVideo.srcObject = stream;
+    }
+  } else {
+    console.error("NO VIDEO TAG FOUND");
+  }
+};
+
+// getMediaDevices();
