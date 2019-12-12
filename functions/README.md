5a4ce2214033204ca28f1922b580fa933d68ca42
Mehrad Kavian <mehrad.kavian@gmail.com>
2019-12-12 10:26:19 -0500

    new heroku account

--- functions/README.md
+++
@@ -1,2 +0,0 @@
-# webRTC-Socket
-A simple p2p chat video chat that uses socket.io as broker server and webRTC for p2p communication.
--- functions/credentials.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-module.exports = {
-
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
